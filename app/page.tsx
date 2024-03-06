1d5defd4ccacb4e971cf8a1c9c6810edace37bb8
chogyejin <whrpwls96@naver.com>
2024-03-07 01:47:56 +0900

    feat: change home text

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 export default function Page() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">my portfolio</h1>
-      <p className="prose prose-neutral dark:prose-invert">
-        This is your new portfolio.
-      </p>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">
+        프론트엔드 개발자 조계진입니다.
+      </h1>
+      <p className="prose prose-neutral dark:prose-invert"></p>
     </section>
   );
 }
