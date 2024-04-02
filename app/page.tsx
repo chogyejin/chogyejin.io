9d6b19b4601363045594a73ad985f92b27f6cdce
chogyejin <whrpwls96@naver.com>
2024-04-02 23:10:49 +0900

    feat: add jsonLd in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,18 @@
 export default function Page() {
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'WebSite',
+    name: 'chogyejin.io',
+    alternateName: ['chogyejin', '조계진'],
+    url: 'https://chogyejin-io.vercel.app/',
+  };
+
   return (
     <section>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
       <h1 className="font-medium text-2xl mb-8 tracking-tighter">
         프론트엔드 개발자 조계진입니다.
       </h1>
