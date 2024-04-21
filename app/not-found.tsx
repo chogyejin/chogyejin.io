3cfe9ad6c7d680d67f5d1144acece0b789e7e0cc
chogyejin <whrpwls96@naver.com>
2024-04-21 21:49:10 +0900

    feat: add 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <section>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">
+        잘못된 접근입니다!
+      </h1>
+    </section>
+  );
+}
